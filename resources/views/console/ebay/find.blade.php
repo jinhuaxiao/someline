dfbd311049fdbcddf93526265c90bbe98ee573d6
xiaojinhua <jinhua2026@gmail.com>
2017-06-03 04:04:43 +0000

    Initial commit

---
+++ resources/views/console/ebay/find.blade.php
@@ -0,0 +1,14 @@
+@extends('console.layout.frame')
+
+@section('content')
+
+    <div class="bg-light lter b-b wrapper-md">
+        <h1 class="m-n font-thin h3">ebay</h1>
+    </div>
+    <div class="wrapper-md">
+
+        <find></find>
+
+    </div>
+
+@endsection
